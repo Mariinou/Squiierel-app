22e415a689da740d063e6ac83bbe1151e498ff3d
Adrien Girardeau <adrigir25@gmail.com>
2015-05-04 17:13:50 +0200

    first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(nom: 'Dupont', prenom: 'Robert', mdp: '1234', email: 'jambon@patate.fr', state: 'true', type: 'Administrateur', role: 'prof')
