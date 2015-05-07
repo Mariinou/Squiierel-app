b94d50a4f9b891b5a2c8d6c6b532a9f730e7aefd
Adrien Girardeau <adrigir25@gmail.com>
2015-05-07 12:00:39 +0200

    correction but still not working

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-    get '/' => 'board#index'
-    get '/' => 'board#admin'
+root 'board#index'
+
+
   # Example resource route with options:
   #   resources :products do
   #     member do
