e713e7ccb4ac4905924a6859ba6e18d737724f05
Adrien Girardeau <adrigir25@gmail.com>
2015-05-20 16:14:48 +0200

    up + bug fixes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout 'mailer'
+    default from: "noreply@squiierrel.com"
+    layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
 default from: 'notifications@example.com'
- 
+
   def welcome_email(user)
     @user = user
     @url  = 'http://localhost:3000/users/sign_in'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+      mail(to: @user.email,from: "noreply@squiierrel.com", subject: 'Welcome to My Awesome Site')
   end
 end
