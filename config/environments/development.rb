2c6911ae0c5dbd0cad3b62eee3185c3e62d33b64
Super-Jojo <joko95@laposte.net>
2015-05-20 10:07:06 +0200

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,15 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
